25bf8e9fb236615f69c548075f1ca3b6b7fe8c37
Evan Nemerson <evan@nemerson.com>
2015-11-05 18:24:19 -0800

    Initial import.

---
+++ README.md
@@ -0,0 +1,40 @@
+# Install Intel C/C++ Compiler on Travis CI
+
+**THIS DOES NOT WORK YET**.  I'm working on it.  If you want to be
+notified when it does work, subscribe to notifications for issue #1.
+
+This project is intended to provide an easy way to use Intel's
+[silent installer](https://software.intel.com/en-us/articles/intel-composer-xe-2015-silent-installation-guide)
+to install ICC on Travis.
+
+This script was originally written for
+[Squash](https://quixdb.github.io/squash).
+
+## Usage
+
+The script requires the `INTEL_SERIAL_NUMBER` environment variable be
+set.  *Do not include your serial number in your repository*.
+Instead, you can take advantage of Travis' support for
+[secure environment variables](http://docs.travis-ci.com/user/encryption-keys/).
+
+To use it, just copy the `install-icc.sh` script to your repository
+(or you can wget the lastest version if you prefer.  Then, just
+execute it.  Once it completes, you should have `icc` in your path.
+
+Once this script is working you can use
+[Squash](https://quixdb.github.io/squash) as an example.
+
+## Restrictions
+
+ICC is *not* free software.  It's proprietary (and expensive).  Intel
+very graciously provides people developing open-source projects with
+free licenses, but there are some restrictions.  Make sure your usage
+complies with their restrictions, and do not abuse it.  Basically,
+don't do anything to screw this up for the rest of us.
+
+## License
+
+To the extent possible under law, the author(s) of this script have
+waived all copyright and related or neighboring rights to this work.
+For details, see the
+[CC0 1.0 Universal Public Domain Dedication](https://creativecommons.org/publicdomain/zero/1.0/) for details.
---
+++ install-icc.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+# Install Intel Parallel Studio on Travis CI
+# Originally written for Squash <https://github.com/quixdb/squash>
+# by Evan Nemerson <evan@nemerson.com>.  For bug reports, please use
+# <https://github.com/nemequ/travis-intel>
+#
+# This script is meant to be used on Travis CI; it hasn't been tested
+# anywhere else.
+#
+# To use it, you must set the INTEL_SERIAL_NUMBER environment variable
+# to your serial number.  You should use Travis' support for secure
+# environment variables to do this; see
+# <http://docs.travis-ci.com/user/encryption-keys/>.
+
+# Default values; these can be changed with command line arguments:
+DESTINATION="/opt/intel"
+TEMPORARY_FILES="$PWD"
+COMPONENTS="\"intel-comp-l-all-vars__noarch;intel-comp-l-all-common__noarch;intel-comp-l-ps-common__noarch;intel-comp-l-all-devel__x86_64;intel-comp-l-ps-devel__x86_64;intel-comp-l-ps-ss-devel__x86_64;intel-comp-all-doc__noarch;intel-comp-ps-ss-doc__noarch;intel-compxe__noarch;intel-compxe-doc__noarch\""
+PHONE_INTEL="no"
+
+while [ $# != 0 ]; do
+    case "$1" in
+	"--dest")
+	    DESTINATION="$2"; shift
+	    ;;
+	"--tmpdir")
+	    TEMPORARY_FILES="$2"; shift
+	    ;;
+	"--components")
+	    COMPONENTS="$2"; shift
+	    ;;
+    esac
+    shift
+done
+
+INSTALLER_NAME=parallel_studio_xe_2016_online.sh
+
+if [ ! -e "${INSTALLER_NAME}" ]; then
+	wget -O "${TEMPORARY_FILES}/${INSTALLER_NAME}" \
+		http://registrationcenter-download.intel.com/akdlm/irc_nas/7997/parallel_studio_xe_2016_online.sh \
+		|| exit 1
+fi
+chmod u+x "${INSTALLER_NAME}"
+
+SILENT_CFG="${TEMPORARY_FILES}/silent.cfg"
+# See https://software.intel.com/en-us/articles/intel-composer-xe-2015-silent-installation-guide
+echo "# Generated silent configuration file" > "${SILENT_CFG}"
+echo "ACCEPT_EULA=accept" >> "${SILENT_CFG}"
+echo "INSTALL_MODE=RPM" >> "${SILENT_CFG}"
+echo "CONTINUE_WITH_OPTIONAL_ERROR=yes" >> "${SILENT_CFG}"
+echo "PSET_INSTALL_DIR=${DESTINATION}" >> "${SILENT_CFG}"
+echo "CONTINUE_WITH_INSTALLDIR_OVERWRITE=yes" >> "${SILENT_CFG}"
+echo "COMPONENTS=${COMPONENTS}" >> "${SILENT_CFG}"
+echo "PSET_MODE=install" >> "${SILENT_CFG}"
+echo "ACTIVATION_SERIAL_NUMBER=${INTEL_SERIAL_NUMBER}" >> "${SILENT_CFG}"
+echo "ACTIVATION_TYPE=serial_number" >> "${SILENT_CFG}"
+echo "PHONEHOME_SEND_USAGE_DATA=${PHONE_INTEL}" >> "${SILENT_CFG}"
+
+"${TEMPORARY_FILES}/${INSTALLER_NAME}" \
+    -t "${TEMPORARY_FILES}" \
+    -s "${SILENT_CFG}" \
+    --cli-mode \
+    --user-mode
+
+# So Travis doesn't die in case of a long download
+# elapsed=0;
+# while kill -0 $! 2>/dev/null; do
+#     sleep 1
+#     elapsed=$(expr $elapsed + 1)
+#     if [ $(expr $elapsed % 60) -eq 0 ]; then
+# 	echo "Still running... (about $(expr $elapsed / 60) minutes so far)."
+#     fi
+# done
