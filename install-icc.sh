059dc48088f4b2a20430fffd06411f8c5a94563c
Evan Nemerson <evan@nemerson.com>
2016-07-01 14:33:35 -0700

    Run dest argument through realpath to get an absolute path.

--- install-icc.sh
+++ install-icc.sh
@@ -42,7 +42,7 @@ add_components() {
 while [ $# != 0 ]; do
     case "$1" in
 	"--dest")
-	    DESTINATION="$2"; shift
+	    DESTINATION="$(realpath "$2")"; shift
 	    ;;
 	"--tmpdir")
 	    TEMPORARY_FILES="$2"; shift
