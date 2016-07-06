1555e5af03f50eaeff0eeb74bc789290a44e7dd2
Jeff Hammond <jeff.science@gmail.com>
2016-07-05 17:33:32 -0700

    add TBB and MKL tests

---
+++ test/testmkl.f90
@@ -0,0 +1,10 @@
+#if !defined(__INTEL_COMPILER)
+#error Not Intel
+#endif
+
+program main
+  use mkl_service
+  implicit none
+  call mkl_set_num_threads(2)
+  write(6,'(a8)') 'Success'
+end program main
